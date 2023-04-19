17c61e57c89f77b9bbc4ee7a1f15bcf2009e500c
Kendrick Lawton <72633834+kendricklawton@users.noreply.github.com>
2023-04-19 10:07:57 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
+import { Text } from "react-native";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { AuthContextProvider } from './contexts/AuthContext';
 import { useAuth } from "./contexts/AuthContext";
 
@@ -12,6 +13,27 @@ import Email from "./screens/Email";
 import Password from "./screens/Password";
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const Home =()=> {
+  return (
+    <Text>
+      Home
+    </Text>
+  );
+}
+
+const Tabs =()=> {
+  return (
+    <Tab.Navigator   
+    screenOptions={{headerShown: false}}
+    >
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Profile" component={Profile} />
+    </Tab.Navigator>
+  );
+}
+
 
 const Navigation = () => {
 
@@ -24,7 +46,11 @@ const Navigation = () => {
           user
             ?
             <>
-              <Stack.Screen name='Profile' component={Profile} />
+              <Stack.Screen name='Tabs' component={Tabs}
+                options={{
+                  title: '',
+                  }}
+              />
               <Stack.Screen name='Name' component={Name} />
               <Stack.Screen name='Email' component={Email} />
               <Stack.Screen name='Password' component={Password} />
